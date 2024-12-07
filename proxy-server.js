bbcb6828cdf2cec7f41382e2250249c339c37009
Esteban Abaroa <estebanabaroa@protonmail.com>
2024-12-07 21:10:54 +0000

    fix gateway error status code

--- proxy-server.js
+++ proxy-server.js
@@ -69,7 +69,9 @@ proxy.on('proxyRes', (proxyRes, req, res) => {
 proxy.on('error', (e, req, res) => {
   console.error(e)
   // if not ended, will hang forever
-  res.end()
+  res.statusCode = 502
+  res.setHeader('Content-Type', 'text/plain')
+  res.end(`502 Bad Gateway: ${e.message}`)
 })
 
 // start server
